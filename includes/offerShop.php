6dd73fe4ac4d89d20868e9c412d40157864d2591
mustafa <mustafa.sak@outlook.fr>
2025-02-22 14:07:10 +0100

    amélioration lisibilité

--- includes/offerShop.php
+++ includes/offerShop.php
@@ -14,29 +14,30 @@ class OfferShop
         if (!$post) {
             return '';
         }
+
         $offer_fields = get_fields($post->ID);
         $shop_object  = $offer_fields['shop'] ?? null;
 
-        if (is_object($shop_object)) {
-            $shop_id     = $shop_object->ID;
-            $shop_name   = get_the_title($shop_id);
-            $shop_logo   = get_field('logo', $shop_id);
-            $offer_title = get_the_title($post->ID);
-
-            $offerShop  = "<div class='offerShop'>";
-            $offerShop .= "<div class='offerShop__image'>";
-            $offerShop .= $shop_logo ? wp_get_attachment_image($shop_logo['ID'], 'full') : '';
-            $offerShop .= "</div>";
-            $offerShop .= "<div class='offerShop__content'>";
-            $offerShop .= "<div class='offerShop__name'><p>" . esc_html($shop_name) . "</p></div>";
-            $offerShop .= "<div class='offerShop__offer'><p>" . esc_html($offer_title) . "</p></div>";
-            $offerShop .= "</div>";
-            $offerShop .= "</div>";
-
-            return $offerShop;
+        if (!is_object($shop_object)) {
+            return '';
         }
 
-        return '';
+        $shop_id     = $shop_object->ID;
+        $shop_name   = get_the_title($shop_id);
+        $shop_logo   = get_field('logo', $shop_id);
+        $offer_title = get_the_title($post->ID);
+
+        $offerShop  = "<div class='offerShop'>";
+        $offerShop .= "  <div class='offerShop__image'>";
+        $offerShop .=     $shop_logo ? wp_get_attachment_image($shop_logo['ID'], 'full') : '';
+        $offerShop .= "  </div>";
+        $offerShop .= "  <div class='offerShop__content'>";
+        $offerShop .= "    <div class='offerShop__name'><p>" . esc_html($shop_name) . "</p></div>";
+        $offerShop .= "    <div class='offerShop__offer'><p>" . esc_html($offer_title) . "</p></div>";
+        $offerShop .= "  </div>";
+        $offerShop .= "</div>";
+
+        return $offerShop;
     }
 
     public function enqueueStyles(): void
