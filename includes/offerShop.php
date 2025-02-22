2b603a9fedea6afa05cd6debbc0e5c0d0360c0b0
mustafa <mustafa.sak@outlook.fr>
2025-02-22 16:22:10 +0100

    fix shop logo

--- includes/offerShop.php
+++ includes/offerShop.php
@@ -15,21 +15,28 @@ class OfferShop
             return '';
         }
 
-        $offer_fields = get_fields($post->ID);
-        $shop_object  = $offer_fields['shop'] ?? null;
+        $offer_id     = $post->ID;
+        $offer_fields = get_fields($offer_id);
 
+        $shop_object  = $offer_fields['shop'] ?? null;
         if (!is_object($shop_object)) {
             return '';
         }
 
         $shop_id     = $shop_object->ID;
-        $shop_name   = get_the_title($shop_id);
+        
         $shop_logo   = get_field('logo', $shop_id);
-        $shop_logo   = $shop_logo ? wp_get_attachment_image($shop_logo['ID'], 'full') : '';
-        $offer_title = get_the_title($post->ID);
+        if (!empty($shop_logo['id'])) {
+            $shop_logo_html = wp_get_attachment_image($shop_logo['id'], 'full');
+        } else {
+            $shop_logo_html = '';
+        }
+
+        $shop_name   = esc_html(get_the_title($shop_id));
+        $offer_title = esc_html(get_the_title($offer_id));
 
         $offerShop  = "<div class='offerShop'>\n";
-        $offerShop .= "  <div class='offerShop__image'>{$shop_logo}</div>\n";
+        $offerShop .= "  <div class='offerShop__image'>{$shop_logo_html}</div>\n";
         $offerShop .= "  <div class='offerShop__content'>\n";
         $offerShop .= "    <div class='offerShop__name'>{$shop_name}</div>\n";
         $offerShop .= "    <div class='offerShop__offer'>{$offer_title}</div>\n";
