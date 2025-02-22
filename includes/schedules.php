0a9f3a565f4fd0017cc0cd15eb8bd8c427dd9d03
mustafa <mustafa.sak@outlook.fr>
2025-02-22 13:17:57 +0100

    rename class

--- includes/schedules.php
+++ includes/schedules.php
@@ -1,6 +1,6 @@
 <?php
 
-class MallSchedules
+class Schedules
 {
     public function __construct()
     {
--- plugin.php
+++ plugin.php
@@ -21,5 +21,5 @@ if (!function_exists('get_field')) {
 require_once __DIR__ . '/includes/schedules.php';
 require_once __DIR__ . '/includes/offer-shop.php';
 
-new MallSchedules();
+new Schedules();
 new OfferShop();
